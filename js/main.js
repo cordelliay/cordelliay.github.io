d5d06e2950c64d160f043be0fc3241c4be80bef9
CordelliaY <cordelliay@hotmail.com>
2019-07-09 19:00:38 -0400

    Updates
    All the updates

--- js/main.js
+++ js/main.js
@@ -1,31 +1,22 @@
-$(function() { 
-   $(".toolgif").toolgif(); 
+AOS.init();
+$(function() {
 
-});
-var wrapper = document.querySelector('.svg-animation');
-
-window.onscroll = function (event) {
-  if (wrapper.getElementById = "draw") {
-    wrapper.className = "on";
-    window.onscroll = null;
-  }
-};
+    $(".toolgif").toolgif();
 
 var skills = [];
-var tech =['#tech', ["CSS3", "HTML5", "MySQL", "PHP", "XML", "JSON", "JQuery", "Javascript", "Propel ORM", "SASS / SCSS", "AngularJS", "Twig", "Highcharts", "Slim Framework", "Memcache", "Bootstrap", "Foundation"]];
-var api = ['#api', ["Altmetrics","Scopus","MailChimp","Asana","YouTube"]];
-var seo = ['#seo', ["Google Analytics","Google Tag Manager","Google Optimize","Webmaster Tools","MozPro"]];
-var other =['#other', ["Filemaker","Bookmaster by Iptor","STATA","Tableau","Neon","AllBooks","Microsoft Access","Google Analytics","Google Tag Manager","Google Optimize",]];
+var tech = ['#tech', ["MySQL", "PHP", "CSS3", "HTML5", "XML", "JSON", "JQuery", "Javascript", "Propel ORM", "SASS / SCSS", "AngularJS", "Twig", "Slim Framework", "Bootstrap", "Foundation"]];
+var api = ['#api', ["Altmetrics", "Scopus", "MailChimp", "Asana", "YouTube", "Google Analytics", "Highcharts"]];
+var seo = ['#seo', ["Google Analytics", "Google Tag Manager", "Google Optimize", "Webmaster Tools", "MozPro"]];
+var other = ['#other', ["Filemaker", "STATA", "Tableau", "AllBooks", "Data Studio", "Google Tag Manager", "Google Optimize", ]];
 skills.push(tech, api, other);
 
 for (var i = skills.length - 1; i >= 0; i--) {
-	var id =  skills[i][0];
-	var items = skills[i][1];
-	for (var c = 0; c < items.length; c++) {
-		$(id).append("<li>"+items[c]+"</li>");
-	}
+    var id = skills[i][0];
+    var items = skills[i][1];
+    for (var c = 0; c < items.length; c++) {
+        $(id).append("<li>" + items[c] + "</li>");
+    }
+
 }
-
-	
-
-
+ 
+});
